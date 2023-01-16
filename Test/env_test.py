056d291969de20805eb4a2bb012b819f7c5b0c4a
Daniel <hacky1610@gmx.de>
2023-01-16 17:21:09 +0100

    Add path resolver

--- Test/env_test.py
+++ Test/env_test.py
@@ -2,21 +2,25 @@ import unittest
 from Envs.stocksEnv import StocksEnv
 from Connectors.Loader import Loader
 from Envs.tradingEnv import Actions,Positions
-
+import pathlib
+import os
 
 class StockEnvTest(unittest.TestCase):
+    def setUp(self):
+        currentDir = pathlib.Path(__file__).parent.resolve()
+        csvPath = os.path.join(currentDir, "Data", "testData.csv")
+        self.df = Loader.loadFromFile(csvPath)
 
     def test_tradingFirstBuy_shouldNotHaveAProfit(self):
-        df = Loader.loadFromFile("./Data/testData.csv")
-        se = StocksEnv(df,2,(2,6))
+
+        se = StocksEnv(self.df,2,(2,6))
         se.reset()
         obs, re, done, info = se.step(Actions.Buy.value)
         assert re == 0
         assert info["total_profit"] == 1.0
 
     def test_tradingSecondBuy_noReward(self):
-        df = Loader.loadFromFile("./Data/testData.csv")
-        se = StocksEnv(df, 2, (2, 6))
+        se = StocksEnv(self.df, 2, (2, 6))
         se.reset()
         se.step(Actions.Buy.value)
         obs, re, done, info = se.step(Actions.Sell.value)
@@ -24,8 +28,7 @@ class StockEnvTest(unittest.TestCase):
         assert 0.98505 == info["total_profit"]
 
     def test_tradingThirdBuy_aReward(self):
-        df = Loader.loadFromFile("./Data/testData.csv")
-        se = StocksEnv(df, 2, (2, 6))
+        se = StocksEnv(self.df, 2, (2, 6))
         se.reset()
         se.step(Actions.Buy.value)
         se.step(Actions.Buy.value)
@@ -34,8 +37,7 @@ class StockEnvTest(unittest.TestCase):
         assert 1.083555 == info["total_profit"]
 
     def test_tradingThirdBuy_negativReward(self):
-        df = Loader.loadFromFile("./Data/testData.csv")
-        se = StocksEnv(df, 2, (2, 10))
+        se = StocksEnv(self.df, 2, (2, 10))
         se.reset()
         se.step(Actions.Buy.value)
         se.step(Actions.Buy.value)
