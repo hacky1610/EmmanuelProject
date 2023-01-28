420a31d59344eb83d5edc0fd9bb3e80b8e5d174a
Daniel <hacky1610@gmx.de>
2023-01-28 21:47:45 +0100

    Fixes

--- evaluate.py
+++ evaluate.py
@@ -1,31 +1,33 @@
-import Utils.Utils
-from Tracing.FileTracer import FileTracer
-from Connectors.Loader import Loader
 from Envs.forexEnv import ForexEnv
 import ray
-from ray import tune
-from datetime import datetime
 from Agents.RayTune import RayTune
 from Connectors.runMetrics import RunMetric,FileHandler
+from Connectors.IG import IG
+from Connectors.tiingo import Tiingo
+from Data.data_processor import DataProcessor
+import Utils.Utils
+from Tracing.FileTracer import FileTracer
 from pathlib import Path
 import os
-from Data.data_processor import DataProcessor
 
-symbol = "GBPUSD=X"
-tracer = FileTracer(os.path.join(Path.home(),"Emmanuel.log"))
+#Variables
+symbol = "GBPUSD"
 dataProcessor = DataProcessor()
-test_df = Loader.loadFromOnline(symbol, datetime(2022, 10, 3), datetime(2023, 12, 20),dataProcessor)
-test_env_conf = RayTune.create_env_config(test_df, 8, tracer)
+tracer = FileTracer(os.path.join(Path.home(),"Emmanuel.log"))
+tiingo = Tiingo()
 
-agTest = RayTune()
-checkpoint =  "/home/daniel/ray_results/PPO/PPO_ForexEnv_12b12_00000_0_2023-01-23_11-02-41/checkpoint_000125"
+#Load Data
+test_df = tiingo.load_data_by_date(symbol, "2023-01-01", "2023-01-10", dataProcessor,"4hour")
+test_env_conf = RayTune.create_env_config(test_df, 12, tracer,0.0009,0.0009)
+
+checkpoint = os.path.join(Utils.Utils.get_project_dir(),"Models/PPO_Win12_4hour_L9_S9_2801")
+#Test
+agTest = RayTune(tracer)
 info = agTest.evaluate(ForexEnv, test_env_conf,checkpoint,Utils.Utils.get_runs_dir())
 
 rm = RunMetric(FileHandler("./runs"))
-
 if rm.is_better_than_last(symbol,info):
     rm.save(symbol,info)
     print("This run is better than before")
 else:
     print("This run is worse than before")
-
