44b7dc7460c84ee5aec607d26cad09d4ce8fd5ef
Daniel Hackbarth <daniel.hackbarth@siemens.com>
2023-01-29 22:48:20 +0100

    Evaluate

--- evaluate.py
+++ evaluate.py
@@ -17,12 +17,12 @@ tracer = FileTracer(os.path.join(Path.home(),"Emmanuel.log"))
 tiingo = Tiingo()
 
 #Load Data
-test_df = tiingo.load_data_by_date(symbol, "2023-01-01", "2023-01-10", dataProcessor,"4hour")
-test_env_conf = RayTune.create_env_config(test_df, 12, tracer,0.0009,0.0009)
+test_df = tiingo.load_data_by_date(symbol, "2023-01-01", "2023-01-10", dataProcessor,"1hour")
+test_env_conf = RayTune.create_env_config(test_df, 8, tracer,0.0009,0.0009)
 
-checkpoint = os.path.join(Utils.Utils.get_project_dir(),"Models/PPO_Win12_4hour_L9_S9_2801")
+checkpoint = os.path.join(Utils.Utils.get_project_dir(),"Models/PPO_Win8_2501")
 #Test
-agTest = RayTune(tracer)
+agTest = RayTune(tracer,algorithm="PPO")
 info = agTest.evaluate(ForexEnv, test_env_conf,checkpoint,Utils.Utils.get_runs_dir())
 
 rm = RunMetric(FileHandler("./runs"))
