fc2b9ee8fe8bebc7f7e1d2af36a34ffdc9b69fe5
Daniel Hackbarth <daniel.hackbarth@siemens.com>
2023-02-21 20:34:28 +0100

    fix

--- report.py
+++ report.py
@@ -3,6 +3,7 @@ from Connectors.tiingo import Tiingo
 from datetime import date, timedelta, datetime
 from Data.data_processor import DataProcessor
 from Logic.analytics import Analytics
+from Tracing.ConsoleTracer import ConsoleTracer
 
 symbol = "GBPUSD"
 tiingo = Tiingo()
@@ -11,9 +12,7 @@ start_time = (datetime.now() - timedelta(hours=24))
 start_time_str = start_time.strftime("%Y-%m-%dT%H:%M:%S")
 
 df = tiingo.load_data_by_date(symbol, start_time.strftime("%Y-%m-%d"),
-                              None, DataProcessor(), "1hour",False,False)
+                              None, DataProcessor(), "1min",False,False)
 
-a = Analytics()
-a.has_peak(df)
 
 ig.create_report(df[df["date"] > start_time_str], symbol, start_time_str)
