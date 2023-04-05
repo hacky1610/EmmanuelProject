4231f2412fe248e54a9c3b9e36feae45c7518426
Daniel <hacky1610@gmx.de>
2023-04-05 09:15:55 +0200

    Fix

--- Connectors/IG.py
+++ Connectors/IG.py
@@ -39,7 +39,7 @@ class IG:
             markets.append({
                 "symbol":market[1].instrumentName.replace("/","").replace(" Kassa",""),
                 "epic":market[1].epic,
-                "spread": (market[1].offer - market[1].bid) * market[1].scalingFactor
+                "spread": (market[1].offer - market[1].bid) * market[1].scalingFactor,
                 "scaling": market[1].scalingFactor
             })
         return markets
