b8b92b97934951b63a67c7def258fb75b7d8f728
Daniel <hacky1610@gmx.de>
2023-04-06 14:16:20 +0200

    Clean

--- trade.py
+++ trade.py
@@ -3,20 +3,15 @@ from Connectors.IG import IG
 from Data.data_processor import DataProcessor
 from Tracing.LogglyTracer import LogglyTracer
 from Connectors.tiingo import Tiingo
-from LSTM_Logic.Utils import read_config
+from BL.utils import read_config
 from BL import Trader, Analytics
 from Predictors import *
-import pandas as pd
-
-# Variables
-stock_list = pd.read_csv("./Data/ForexList.csv")
-stock_list.set_index(stock_list.currency_pair_code, inplace=True)
 
 dataProcessor = DataProcessor()
 config = read_config()
 tracer = LogglyTracer(config["loggly_api_key"])
 tiingo = Tiingo()
-ig = IG(tracer, stock_list)
+ig = IG(tracer)
 
 trader = Trader(
     ig=ig,
@@ -26,10 +21,9 @@ trader = Trader(
     dataprocessor=dataProcessor,
     analytics=Analytics(tracer))
 
-
 while True:
     markets = ig.get_markets()
     for market in markets:
-        trader.trade(market["symbol"],market["epic"],market["spread"],market["scaling"])
+        trader.trade(market["symbol"], market["epic"], market["spread"], market["scaling"])
 
     time.sleep(60 * 60)
