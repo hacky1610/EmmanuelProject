4f687aebb88d9e6ca6c6749cf2d9dd5df07f05bc
Daniel <hacky1610@gmx.de>
2023-04-15 23:03:50 +0200

    Müde

--- trade_once.py
+++ trade_once.py
@@ -20,28 +20,29 @@ dataProcessor = DataProcessor()
 tracer = LogglyTracer(env_reader.get("loggly_api_key"), type_)
 tiingo = Tiingo(tracer=tracer, conf_reader=env_reader)
 ig = IG(conf_reader=env_reader, tracer=tracer, live=live)
-exclude = ["GBPAUD","EURAUD","EURSGD","AUDUSD"]
+exclude = ["CHFJPY", "GBPJPY", "EURCHF"]
+predictor = RsiStoch({})
 
 trader = Trader(
     ig=ig,
     tiingo=tiingo,
     tracer=tracer,
-    predictor=RsiStoch({}),
+    predictor=predictor,
     dataprocessor=dataProcessor,
     analytics=Analytics(tracer))
 
-
-#trade
+# trade
 markets = ig.get_markets()
+tracer.write(f"Trade with settings {predictor.get_config_as_string()}")
 for market in markets:
     symbol = market["symbol"]
     if symbol not in exclude:
         tracer.write(f"Try to trade {symbol}")
         trader.trade(market["symbol"], market["epic"], market["spread"], market["scaling"])
 
-#report
+# report
 if datetime.now().hour == 18:
     tracer.write("Create report")
     dbx = dropbox.Dropbox(env_reader.get("dropbox"))
-    ds = DropBoxService(dbx,type_)
-    ig.create_report(tiingo,ds)
+    ds = DropBoxService(dbx, type_)
+    ig.create_report(tiingo, ds)
