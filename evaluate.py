e5616b56d602efc64777093b74d27f9df636903d
Daniel <hacky1610@gmx.de>
2023-04-15 23:05:02 +0200

    Remove params

--- evaluate.py
+++ evaluate.py
@@ -20,7 +20,7 @@ for m in ig.get_markets(tradebale=False):
         df = pd.read_csv(f"./Data/{symbol}_1hour.csv", delimiter=",")
         df_eval = pd.read_csv(f"./Data/{symbol}_5min.csv", delimiter=",")
         df_eval.drop(columns=["level_0"], inplace=True)
-        reward, success, trade_freq, win_loss, avg_minutes  = evaluate(RsiStoch({"df": df, "df_eval": df_eval}), df, df_eval, False)
+        reward, success, trade_freq, win_loss, avg_minutes  = evaluate(RsiStoch({}), df, df_eval, False)
 
         print(f"{symbol} - Reward {reward}, success {reward}, trade_freq {trade_freq}, win_loss {win_loss} avg_minutes {avg_minutes}")
     except Exception:
