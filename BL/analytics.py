7edec7d6198d5ba56b0009a4801efa91188f8fac
Daniel <hacky1610@gmx.de>
2023-04-29 09:42:06 +0200

    clean

--- BL/analytics.py
+++ BL/analytics.py
@@ -11,29 +11,6 @@ class Analytics:
     def __init__(self, tracer: Tracer = ConsoleTracer()):
         self._tracer = tracer
 
-    def has_peak(self, df: DataFrame, lockback: int = 3, max_limit: float = 2.5):
-        mean_diff = (df["high"] - df["low"]).mean()
-
-        max = (df[lockback * -1:]["high"] - df[lockback * -1:]["low"]).max()
-
-        if max > mean_diff * max_limit:
-            self._tracer.debug(
-                f"Peak of {max} compared to mean {mean_diff}. Limit times {max_limit} im the last {lockback} dates")
-            return True
-
-        return False
-
-    def is_sleeping(self, df: DataFrame, lockback: int = 2, max_limit: float = 0.6):
-        mean_diff = (df["high"] - df["low"]).mean()
-        m = (df[lockback * -1:]["high"] - df[lockback * -1:]["low"]).mean()
-
-        if m < mean_diff * max_limit:
-            #
-            self._tracer.debug(f"No movement {m} in the last {lockback} dates")
-            return True
-
-        return False
-
     def evaluate(self, predictor, df_train: DataFrame, df_eval: DataFrame, viewer: BaseViewer = BaseViewer()):
         reward = 0
         losses = 0
