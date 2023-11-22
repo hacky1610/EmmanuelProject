df1bb19f16adc97c1ee25dde9d8aa6505b7321a2
hacky1610 <hacky1610@gmx.de>
2023-11-22 12:44:09 +0000

    switch config reader

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from BL import ConfigReader
+from BL import ConfigReader, EnvReader
 from zulu_trade import trade
 import sys
+import os
+
+
+def check_config_folder():
+
+    path_to_config = os.path.join(os.path.dirname(__file__), "Config")
+    return os.path.exists(path_to_config) and os.path.isdir(path_to_config)
+ 
+
 
 def main():
     # Zugriff auf die Befehlszeilenargumente
@@ -10,7 +19,11 @@ def main():
         account_type = "DEMO"
     else:
         account_type = arguments[1]
-    conf_reader = ConfigReader(account_type)
+
+    if check_config_folder():
+        conf_reader = ConfigReader(account_type)
+    else:
+        conf_reader = EnvReader()
 
     print(f"Trade {account_type} account")
     trade(conf_reader, account_type)
