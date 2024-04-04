bb44e13d88aee86e18753217cec122b3b28ca186
Daniel Hackbarth <daniel.hackbarth@siemens.com>
2024-04-04 12:53:46 +0200

    Add mode

--- Tracing/LogglyTracer.py
+++ Tracing/LogglyTracer.py
@@ -1,29 +1,34 @@
 from Tracing.Tracer import Tracer
 from Tracing.Loggly.connection import LogglyConnection
 
+
 class LogglyTracer(Tracer):
 
-
-    def __init__(self,token:str,type:str, prefix:str= ""):
+    def __init__(self, token: str, type: str, mode: str = ""):
         self._log = LogglyConnection(token)
         self.type = type
-        self._prefix: str = prefix
-
+        self._prefix: str = ""
+        self._mode: str = mode
 
     def write(self, message):
-        self._log.create_input(f"[TI {self.type}] | INFO | {self._prefix} | {self._get_function()} | {message}")
+        self._log.create_input(
+            f"[TI {self.type}] | {self._mode} | INFO | {self._prefix} | {self._get_function()} | {message}")
 
     def info(self, message):
-        self._log.create_input(f"[TI {self.type}] | INFO | {self._prefix} | {self._get_function()} | {message}")
+        self._log.create_input(
+            f"[TI {self.type}] | {self._mode} | INFO | {self._prefix} | {self._get_function()} | {message}")
 
     def debug(self, message):
-        self._log.create_input(f"[TI {self.type}] | DEBUG | {self._prefix} | {self._get_function()} | {message}")
+        self._log.create_input(
+            f"[TI {self.type}] | {self._mode} | DEBUG | {self._prefix} | {self._get_function()} | {message}")
 
     def error(self, message):
-        self._log.create_input(f"[TI {self.type}] | ERROR | {self._prefix} | {self._get_function()} | {message}")
+        self._log.create_input(
+            f"[TI {self.type}] | {self._mode} | ERROR | {self._prefix} | {self._get_function()} | {message}")
 
     def warning(self, message):
-        self._log.create_input(f"[{self.type}] | WARNING | {self._prefix} | {self._get_function()} | {message}")
+        self._log.create_input(
+            f"[{self.type}] | {self._mode} | WARNING | {self._prefix} | {self._get_function()} | {message}")
 
     def result(self, message):
         print("Result: {}:".format(message))
