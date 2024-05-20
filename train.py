b15f12a8947b282edf06edd364bbd02242c17176
Daniel Hackbarth <daniel.hackbarth@siemens.com>
2024-05-20 09:51:11 +0200

    no cache

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ else:
 
 dbx = dropbox.Dropbox(conf_reader.get("dropbox"))
 ds = DropBoxService(dbx, type_)
-cache = DropBoxCache(ds, use_local_cache=True)
+cache = DropBoxCache(ds)
 client = pymongo.MongoClient(f"mongodb+srv://emmanuel:{conf_reader.get('mongo_db')}@cluster0.3dbopdi.mongodb.net/?retryWrites=true&w=majority")
 db = client["ZuluDB"]
 ms = MarketStore(db)
