0eef14360a743ac06ee6dc4dae2b2d9cb94abb29
Daniel <hacky1610@gmx.de>
2024-08-13 17:59:52 +0200

    improve bash

--- train_matrix.sh
+++ train_matrix.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-# Pfad zum Python-Interpreter
-PYTHON_INTERPRETER="~/Documents/Projects/EmmanuelProject/venv/bin/python"
+# Name des Programms, das gestartet werden soll
+PROGRAMM="python train_matrix.py"
 
-# Name des Python-Skripts
-PYTHON_SCRIPT="train_matrix.py"
 
-# Anzahl der Wiederholungen
-NUM_RUNS=10
+# Anzahl der Aufrufe
+ANZAHL=10
 
-# Zeit in Sekunden zwischen den Aufrufen
-SLEEP_TIME=60
+# Anzahl der verfügbaren CPU-Kerne (Ermittle die Anzahl der Kerne im System)
+KERNANZAHL=$(nproc)
 
-# Schleife für die Ausführung des Python-Skripts
-for ((i=1; i<=NUM_RUNS; i++))
+for i in $(seq 0 $((ANZAHL-1)))
 do
-  echo "Ausführung $i von $NUM_RUNS"
-  python $PYTHON_SCRIPT &
-  if [ $i -lt $NUM_RUNS ]; then
-    echo "Warten für $SLEEP_TIME Sekunden..."
-    sleep $SLEEP_TIME
-  fi
-done
+  # Berechne den CPU-Kern, auf dem dieser Aufruf laufen soll (Modulo-Operation)
+  KERN=$((i % KERNANZAHL))
 
-echo "Alle $NUM_RUNS Ausführungen abgeschlossen."
+  echo "Starte $PROGRAMM auf CPU-Kern $KERN"
+
+  # Starte das Programm auf dem entsprechenden Kern
+  taskset -c $KERN $PROGRAMM &
+
+  # Optional: Warten, bis der vorherige Prozess abgeschlossen ist (entfernen, wenn parallele Ausführung gewünscht ist)
+  sleep 10
+done
