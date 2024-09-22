af3644cc3d026882b3dbb7610f0d5ebf7aafa4b2
Daniel Hackbarth <daniel.hackbarth@siemens.com>
2024-09-22 18:17:32 +0200

    onl error

--- Tracing/StreamTracer.py
+++ Tracing/StreamTracer.py
@@ -4,18 +4,22 @@ from Tracing.Tracer import Tracer
 import inspect
 class StreamTracer(Tracer):
 
-    def __init__(self):
+    def __init__(self, verbose = False):
         logging.basicConfig(level=logging.INFO)
         self._logger = logging.getLogger(__name__)
+        self._verbose = verbose
 
     def info(self, message):
-        self._logger.info(f"{super()._get_function()} - {message}")
+        if self._verbose:
+            self._logger.info(f"{super()._get_function()} - {message}")
 
     def debug(self, message):
-        self._logger.info(f"{super()._get_function()} - {message}")
+        if self._verbose:
+            self._logger.info(f"{super()._get_function()} - {message}")
 
     def write(self, message):
-        self._logger.info(f"{super()._get_function()} - {message}")
+        if self._verbose:
+            self._logger.info(f"{super()._get_function()} - {message}")
 
     def error(self, message):
         self._logger.error(f"EmmanuelError:{super()._get_function()} - {message}:")
