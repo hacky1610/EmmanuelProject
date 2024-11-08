ed4e4f6b58b28c4f08d867cef595ec33c0001f2e
Daniel Hackbarth <daniel.hackbarth@siemens.com>
2024-11-08 19:57:09 +0100

    Train period

--- Connectors/dropbox_cache.py
+++ Connectors/dropbox_cache.py
@@ -113,7 +113,7 @@ class DropBoxCache(BaseCache):
 
         # Kalenderwoche abrufen
         day_of_year = heute.timetuple().tm_yday
-        index = day_of_year - (day_of_year % 10)
+        index = day_of_year - (day_of_year % 3)
 
         return f"Training_V21/{heute.year}_D{index}"
 
