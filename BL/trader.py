98386fc65c527f71f8fb47ff8b77b888804e4443
Daniel Hackbarth <daniel.hackbarth@siemens.com>
2025-04-16 19:51:51 +0200

    foo

--- BL/trader.py
+++ BL/trader.py
@@ -384,11 +384,11 @@ class Trader:
         signal = predictor.predict(buy_actions_df, sell_actions_df)
         #signal = TradeAction.SELL
         market = self._market_store.get_market(config.symbol)
-        stop = trade_df.ATR.iloc[-1] * 1.33 * config.scaling
-        limit = trade_df.ATR.iloc[-1] * 0.5 * config.scaling
+        stop = trade_df.ATR.iloc[-1] * predictor.get_atr_factor_stop() * config.scaling
+        limit = trade_df.ATR.iloc[-1] * predictor.get_atr_factor_limit() * config.scaling
 
         if signal == TradeAction.NONE or signal == TradeAction.BOTH:
-            return TradeResult.NOACTION
+                return TradeResult.NOACTION
 
         is_manual_stop = False
         minimal_stop = self._ig.get_min_stop_distance(config.epic)
