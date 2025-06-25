7efa0d5ae1490235b9e6a12fcf71614c00235eb3
Daniel <hacky1610@gmx.de>
2025-06-25 16:05:31 +0200

    foo

--- BL/combination_trainer.py
+++ BL/combination_trainer.py
@@ -184,7 +184,7 @@ class CombinationTrainer:
             combos = existing_combos + combos
 
 
-        for features in combos:
+        for features in tqdm(combos):
             try:
                 train_precision, train_reward, trade_indexes_train, trade_count_train = self._predict_sum(train_df,
                                                                                                           features,atr_factor_stop,atr_factor_limit)
