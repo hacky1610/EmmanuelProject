653ab8f58820f0b4394c4e504b4ec4f6c6a9c2ec
Daniel <hacky1610@gmx.de>
2025-07-01 08:19:09 +0200

    New day trade

--- Predictors/deep_predictor.py
+++ Predictors/deep_predictor.py
@@ -184,9 +184,8 @@ class DeepPredictor(BasePredictor):
 
         trades = actions_df[self._features].sum(axis=1) == len(self._features)
         if (actions_df[self._features].sum(axis=1).iloc[0] + 2) >= len(self._features):
-            self._tracer.debug(f"Close to trade {actions_df[self._features].sum(axis=1).iloc[0]} from {len(self._features)}")
-            self._tracer.debug(
-                f"Features {actions_df[self._features]}")
+            self._tracer.debug(f"Close to trade {actions_df[self._features].sum(axis=1).iloc[0]} from {len(self._features)} "
+                               f"Features {actions_df[self._features]}")
         if trades.iloc[0]:
             return self._trade_mode
         return TradeAction.NONE
